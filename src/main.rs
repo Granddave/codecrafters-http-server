31d05ac85d08b1b77968a52578667cf2a83659c1
David Isaksson <davidisaksson93@gmail.com>
2023-09-20 19:29:49 +0200

    Restructure HTTP

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,86 @@
+use std::collections::HashMap;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
 use itertools::Itertools;
+
+#[derive(PartialEq)]
+enum HTTPMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+}
+
+struct HTTPRequest {
+    method: HTTPMethod,
+    path: String,
+    version: String,
+    headers: HashMap<String, String>,
+}
+
+impl HTTPRequest {
+    fn parse(request: &str) -> Option<HTTPRequest> {
+        let lines = request.lines().collect_vec();
+        let start_line = lines[0].split_whitespace().collect_vec();
+        Some(HTTPRequest {
+            method: match start_line[0] {
+                "GET" => HTTPMethod::GET,
+                "POST" => HTTPMethod::POST,
+                "PUT" => HTTPMethod::PUT,
+                "DELETE" => HTTPMethod::DELETE,
+                _ => None?,
+            },
+            path: start_line[1].to_string(),
+            version: start_line[2].to_string(),
+            headers: lines[1..]
+                .iter()
+                .map(|x| {
+                    let mut parts = x.splitn(2, ": ");
+                    (
+                        parts.next().unwrap().to_string(),
+                        parts.next().unwrap().to_string(),
+                    )
+                })
+                .collect(),
+        })
+    }
+}
+
+struct HTTPResponse {
+    status_code: u16,
+    status_text: String,
+    headers: HashMap<String, String>,
+    body: String,
+}
+
+impl HTTPResponse {
+    fn to_string(&self) -> String {
+        let headers = self
+            .headers
+            .iter()
+            .map(|(k, v)| format!("{}: {}", k, v))
+            .join("\r\n");
+        format!(
+            "HTTP/1.1 {} {}\r\n{}\r\n\r\n{}",
+            self.status_code, self.status_text, headers, self.body
+        )
+    }
+
+    fn ok() -> HTTPResponse {
+        HTTPResponse {
+            status_code: 200,
+            status_text: "OK".to_string(),
+            headers: HashMap::new(),
+            body: "".to_string(),
+        }
+    }
+}
+
+fn send_response(mut stream: TcpStream, response: HTTPResponse) {
+    stream.write(response.to_string().as_bytes()).unwrap();
+}
 
 fn handle(mut stream: TcpStream) {
     println!("accepted new connection");
@@ -10,32 +88,27 @@ fn handle(mut stream: TcpStream) {
     let request = &mut [0; 512];
     stream.read(request).unwrap();
     let text = String::from_utf8_lossy(request).to_string();
-    let lines = text.lines().collect_vec();
-    let start_line = lines[0].split_whitespace().collect_vec();
-    if start_line[0] == "GET" {
-        match start_line[1] {
-            "/" => {
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
-                stream.write(response.as_bytes()).unwrap();
-            }
+    let request = HTTPRequest::parse(&text).unwrap();
+    if request.method == HTTPMethod::GET {
+        match request.path.as_str() {
+            "/" => send_response(stream, HTTPResponse::ok()),
             "/user-agent" => {
-                let headers = lines[1..].iter().map(|x| x.to_string()).collect_vec();
-                let user_agent = headers
-                    .iter()
-                    .find(|x| x.starts_with("User-Agent: "))
-                    .unwrap()
-                    .replace("User-Agent: ", "")
-                    .to_string();
-                let res = format!(
-                    "Content-type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                    user_agent.len(),
-                    user_agent,
+                let user_agent = request.headers.get("User-Agent").unwrap();
+                send_response(
+                    stream,
+                    HTTPResponse {
+                        status_code: 200,
+                        status_text: "OK".to_string(),
+                        headers: HashMap::from([
+                            ("Content-type".to_string(), "text/plain".to_string()),
+                            ("Content-Length".to_string(), user_agent.len().to_string()),
+                        ]),
+                        body: user_agent.to_string(),
+                    },
                 );
-                let response = format!("HTTP/1.1 200 OK\r\n{}", res);
-                stream.write(response.as_bytes()).unwrap();
             }
-            _ if start_line[1].starts_with("/echo/") => {
-                let echo = start_line[1].replace("/echo/", "");
+            _ if request.path.starts_with("/echo/") => {
+                let echo = request.path.replace("/echo/", "");
                 let res = format!(
                     "Content-type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
                     echo.len(),
