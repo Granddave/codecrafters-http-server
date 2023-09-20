857bf4a8af65fa454af788bd7753336f53d99d06
David Isaksson <davidisaksson93@gmail.com>
2023-09-20 18:30:10 +0200

    Handle GET /

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 use std::io::{Read, Write};
 use std::net::TcpListener;
 
+use itertools::Itertools;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut data) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
-                data.read(&mut [0; 128]).unwrap();
-                data.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+
+                let request = &mut [0; 512];
+                stream.read(request).unwrap();
+                let text = String::from_utf8_lossy(request).to_string();
+                let lines = text.lines().collect_vec();
+                let start_line = lines[0].split_whitespace().collect_vec();
+                if start_line[0] == "GET" {
+                    match start_line[1] {
+                        "/" => {
+                            let response = "HTTP/1.1 200 OK\r\n\r\n";
+                            stream.write(response.as_bytes()).unwrap();
+                        }
+                        _ => {
+                            let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+                            stream.write(response.as_bytes()).unwrap();
+                        }
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
